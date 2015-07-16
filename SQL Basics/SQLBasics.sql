3fd8f97d86731674afcd432a640b38d86744acb1
Nipun Chawla <ncnipunchawla@gmail.com>
2015-07-16 19:27:26 +0530

    SQL Functions and Advance Queries

--- SQL Basics/SQLBasics.sql
+++ SQL Basics/SQLBasics.sql
@@ -134,43 +134,55 @@ ON Employee.EmployeeId=EmployeeSlab.EmployeeId;
 
 --Query 22
 
+
 CREATE TABLE Department
 (
   DepartmentId INT PRIMARY KEY,
-  EmployeeId INT,
   DepartmentName VARCHAR(50) NOT NULL,
 );
 
 INSERT INTO Department
-VALUES(1001,101,'IT');
+VALUES(1001,'IT');
 INSERT INTO Department
-VALUES(1002,102,'');
+VALUES(1002,'HR');
 INSERT INTO Department
-VALUES(1003,103,'CS');
-INSERT INTO Department
-VALUES(1004,104,'CS');
-INSERT INTO Department
-VALUES(1005,105,'HR');
+VALUES(1003,'CS');
 
-DELETE FROM Department WHERE EmployeeId=105;
-DELETE FROM Department WHERE EmployeeId=102;
+SELECT * FROM Department;
+
+ALTER TABLE Employee 
+ADD FOREIGN KEY(DepartmentId)
+REFERENCES Department(DepartmentId);
+
+
+UPDATE Employee
+SET DepartmentId = 1001 WHERE EmployeeId=101;
+UPDATE Employee
+SET DepartmentId = 1001 WHERE EmployeeId=102;
+UPDATE Employee
+SET DepartmentId = 1002 WHERE EmployeeId=103;
+UPDATE Employee
+SET DepartmentId = 1002 WHERE EmployeeId=104;
+UPDATE Employee
+SET DepartmentId = 1003 WHERE EmployeeId=105;
+
 
 
 SELECT Employee.FirstName,Department.DepartmentName FROM Employee 
 LEFT OUTER JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId;
+ON Employee.DepartmentId=Department.DepartmentId;
 
 --Query 23
 
 SELECT Employee.FirstName,Department.DepartmentName FROM Employee 
 RIGHT OUTER JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId;
+ON Employee.DepartmentId=Department.DepartmentId;
 
 --Query 24
 
 SELECT Employee.FirstName,Department.DepartmentName FROM Employee 
 FULL OUTER JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId;
+ON Employee.DepartmentId=Department.DepartmentId;
 
 --Query 25
 
@@ -317,167 +329,3 @@ UPDATE Employee
 SET Pf = Salary*.1275;
 
 SELECT ROUND(Pf,2) FROM Employee;
-
---Query 34
-
-SELECT * FROM Employee
-WHERE Salary > (SELECT AVG(Salary) FROM Employee);
-
---Query 35
-
-SELECT Department.DepartmentName,COUNT(Employee.EmployeeId) 
-AS NumberOfEmployee FROM Employee
-JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId
-GROUP BY DepartmentName;
-
---Query 36
-
-SELECT * FROM Employee
-WHERE Salary < (SELECT MAX(Salary) FROM Employee); 
-
---Query 37
-
-SELECT * FROM Employee
-WHERE Salary < (SELECT MIN(Salary) FROM Employee); 
-
---Query 38
-
-SELECT SUM(Salary) FROM Employee;
-
---Query 39
-
-SELECT Department.DepartmentName,COUNT(Employee.EmployeeId) 
-AS NumberOfEmployee FROM Employee
-JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId
-GROUP BY DepartmentName;
-
---Query 41
-
-SELECT FirstName,UPPER(LastName) FROM Employee;
-
---Query 42
-
-SELECT FirstName,LOWER(LastName) FROM Employee;
-
---Query 43
-
-SELECT LEN(FirstName) AS Length FROM Employee;
-
---Query 44
-
-CREATE TABLE EmployeeSalary
-(
-  EmployeeSalary INT,
-  EmployeeName VARCHAR(50) NOT NULL
-);
-
-INSERT INTO EmployeeSalary
-VALUES(25000.99,'Nipun');
-INSERT INTO EmployeeSalary
-VALUES(35000.89,'Rishabh');
-INSERT INTO EmployeeSalary
-VALUES(45000,'Swati');
-INSERT INTO EmployeeSalary
-VALUES(55000.50,'Aastha');
-INSERT INTO EmployeeSalary
-VALUES(65000,'Vaishali');
-
-SELECT EmployeeName,ROUND(EmployeeSalary,0) FROM EmployeeSalary;
-
---Query 45
-
-SELECT *,GETDATE() AS CurrentDate FROM Employee;
-
---Query 46
-
-SELECT FirstName,Salary,CONVERT(VARCHAR(20),GETDATE(),105) AS Today,
-CONVERT(VARCHAR(19),GETDATE(),0) AS Today,
-CONVERT(VARCHAR(10),GETDATE(),10) AS Today,
-CONVERT(VARCHAR(10),GETDATE(),110) AS Today,
-CONVERT(VARCHAR(11),GETDATE(),6) AS Today,
-CONVERT(VARCHAR(11),GETDATE(),106) AS Today,
-CONVERT(VARCHAR(24),GETDATE(),113) AS Today FROM Employee;
-
---Query 47
-
-SELECT CAST(EmployeeId AS VARCHAR(10)) AS EmpId FROM Employee;
-
---Query 48
-
-SELECT CASE
-		WHEN Salary>=50000 AND Age<35 THEN 'Yes'
-		ELSE 'No'
-		END
-FROM Employee;
-
-
---Query 49
-
-SELECT * FROM (SELECT FirstName,LastName,Salary,RANK()
-OVER(ORDER BY Salary DESC) AS Rank FROM Employee) 
-AS Temp  WHERE Temp.Rank < 4;
-
-SELECT * FROM (SELECT FirstName,LastName,Salary,ROW_NUMBER()
-OVER(ORDER BY Salary DESC) AS AlternateRank FROM Employee) 
-AS Temp  WHERE Temp.AlternateRank %2 = 1;
-
---Query 50
-
-WITH EmployeeCTE (FirstName,LastName,Salary)
-AS(SELECT FirstName,LastName,Salary FROM Employee)
-SELECT * FROM EmployeeCTE;
-
---Query 51
-
-SELECT Department.DepartmentName,COUNT(Employee.EmployeeId)
-AS NumberOfEmployee,SUM(Salary) 
-FROM Employee
-JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId
-GROUP BY DepartmentName,Employee.EmployeeId WITH ROLLUP;
-
-SELECT Department.DepartmentName,COUNT(Employee.EmployeeId)
-AS NumberOfEmployee,SUM(Salary) 
-FROM Employee
-JOIN Department
-ON Employee.EmployeeId=Department.EmployeeId
-GROUP BY DepartmentName,Employee.EmployeeId WITH CUBE;
-
---Query 52
-
-ALTER TABLE Employee
-ADD Experience int;
-
-UPDATE Employee
-SET Experience = 3 WHERE EmployeeId = 101;
-UPDATE Employee
-SET Experience = 5 WHERE EmployeeId = 102;
-UPDATE Employee
-SET Experience = 7 WHERE EmployeeId = 103;
-UPDATE Employee
-SET Experience = 9 WHERE EmployeeId = 104;
-UPDATE Employee
-SET Experience = 11 WHERE EmployeeId = 105;
-
-SELECT * FROM Employee
-INTERSECT
-SELECT * FROM Employee WHERE Experience < 6;
-
-SELECT * FROM Employee
-EXCEPT
-SELECT * FROM Employee WHERE Experience >= 6;
-
---Query 53
-
-SELECT FirstName,Salary,ROW_NUMBER()OVER(ORDER BY Salary) AS Row,(
-SELECT 
-COUNT(*) AS CountRow FROM Employee) as C from Employee
-WHERE Row > c.CountRow-3;
-
-
-
-
-
-
